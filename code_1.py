0ed1d5d69d07691ba623c3f983957739fb350140
endoscopysubennj <139906984+endoscopysubennj@users.noreply.github.com>
2023-12-06 09:46:26 -0600

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,27 +1,51 @@
-        print(f"Random Number: {item}")
 if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-
-
 
 def main():
-    for item in data:
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+
+    return data
 import random
-    return data
-    data = generate_random_data()
+    for item in data:
+    main()        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
 
-    main()import random
-    return data
-def generate_random_data():
-        print(f"Random Number: {item}")
-
-    data = generate_random_data()
-if __name__ == "__main__":
-
+public class RandomDataGenerator {
+        int[] data = new int[10];
+        }
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+}
+    }
+        }
 
 def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-
+if __name__ == "__main__":
     main()
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+        print(f"Random Number: {item}")
+    return data
+
     for item in data:
+
+def generate_random_data():
+
+    data = generate_random_data()
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+        }
+
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+        for (int item : data) {
+public class RandomDataGenerator {
+        }
+    public static void main(String[] args) {
+    }
