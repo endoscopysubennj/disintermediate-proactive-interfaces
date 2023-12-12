6ba59934332746b82b86b2739bfd2bdfb3b27b55
endoscopysubennj <139906984+endoscopysubennj@users.noreply.github.com>
2023-12-11 22:45:37 -0600

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,25 @@
+    }
+        }
+
+    public static void main(String[] args) {
+        for (int item : data) {
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+        }
+        for (int i = 0; i < 10; i++) {
+
+        int[] data = new int[10];
+}        }
+            data[i] = (int) (Math.random() * 100) + 1;
+
+
+        for (int i = 0; i < 10; i++) {
+        }
+    public static void main(String[] args) {
+        for (int item : data) {
+        int[] data = new int[10];
+}
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+    }
